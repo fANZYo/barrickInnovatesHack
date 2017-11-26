a953e7d7b84f96daba0b0b99a06b1bea0340105b
yanzhongsu <yanzh.su@gmail.com>
2017-11-26 13:37:02 +0000

    update gold_app.py

--- datasci/gold_app.py
+++ datasci/gold_app.py
@@ -1,6 +1,7 @@
 import flask
 import pickle
 import numpy as np
+import json
 app = flask.Flask(__name__)
 
 #Model
@@ -21,23 +22,32 @@ app = flask.Flask(__name__)
 @app.route('/result')
 def result():
     '''Gets prediction using the HTML form'''
-    if flask.request.method == 'POST':
+    # if flask.request.method == 'POST':
 
-       inputs = flask.request.form
+    #    inputs = flask.request.form
 
-       picture = inputs['picture']
+    #    picture = inputs['picture']
 
 
 
 #       item = np.array()
 #       item = item.reshape(1, -1)
 #       score = PREDICTOR.predict_proba(item)
-       results = {'gold':0.624}
-       return flask.jsonify(results)
+    results = {'gold':0.624}
+    # resp = flask.Response(flask.jsonify(results))
+    # resp.headers['Cross-origin'] = '*'
+    resp = app.response_class(
+    response=json.dumps(results),
+    status=200,
+    mimetype='application/json'
+    )
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    # return flask.jsonify(results)
+    return resp
 
 
 if __name__ == '__main__':
-    HOST = '127.0.0.1'
+    HOST = '0.0.0.0'
     PORT = 4000
 
     app.run(HOST, PORT, debug =True)
